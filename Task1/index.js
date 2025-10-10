35aff693f76e205fa06b63b1b8ef19eb90d2d451
luonggiotaiii <caodinhluong04@gmail.com>
2025-10-10 19:52:46 +0700

    new

--- Task1/index.js
+++ Task1/index.js
@@ -2,21 +2,21 @@ function findElement(arr, target) {
   let left = 0, right = arr.length - 1;
 
   while (left <= right) {
-    const mid = Math.floor((left + right) / 2); 
-    if (arr[mid] === target) return "YES";      
-    if (arr[mid] < target) left = mid + 1;      
-    else right = mid - 1;                       
+    const mid = Math.floor((left + right) / 2);
+    if (arr[mid] === target) return "YES";
+    if (arr[mid] < target) left = mid + 1;
+    else right = mid - 1;
   }
 
-  return "NO"; 
+  return "NO";
 }
 
 
-console.log(findElement([1, 3, 5, 7, 9], 5)); //YES(target nằm ở giữa)
-console.log(findElement([1, 3, 5, 7, 9], 4)); //NO (target không tồn tại)
-console.log(findElement([], 1));              //NO (mảng rỗng)
-console.log(findElement([2, 2, 2, 2], 2));    //YES(tất cả phần tử giống target)
-console.log(findElement([1, 2, 3, 4, 5], 6)); //NO (target lớn hơn max)
+console.log(findElement([1, 3, 5, 7, 9], 5));
+console.log(findElement([1, 3, 5, 7, 9], 4));
+console.log(findElement([], 1));
+console.log(findElement([2, 2, 2, 2], 2));
+console.log(findElement([1, 2, 3, 4, 5], 6));
 
 
 module.exports = findElement;
--- Task2/index.js
+++ Task2/index.js
@@ -31,9 +31,9 @@ function bestHarvest(yields) {
   return bestLength;
 }
 
-console.log(bestHarvest([-2, 1, -3, 4, -1]));        //1(dãy con tốt nhất là [4])
-console.log(bestHarvest([-1, 2, 3, -2, 5, -3]));     //4(dãy con tốt nhất là [2,3,-2,5] có tổng 8)
-console.log(bestHarvest([1, 2, 3, 4]));              //4(cả mảng đều tốt nhất)
-console.log(bestHarvest([-5, -2, -3]));              //1(toàn số âm)
-console.log(bestHarvest([5, -1, 2, -1, 2, -1, 5]));  //7(cả mảng cộng lại là lớn nhất)
+console.log(bestHarvest([-2, 1, -3, 4, -1]));      
+console.log(bestHarvest([-1, 2, 3, -2, 5, -3]));     
+console.log(bestHarvest([1, 2, 3, 4]));              
+console.log(bestHarvest([-5, -2, -3]));              
+console.log(bestHarvest([5, -1, 2, -1, 2, -1, 5]));  
 module.exports = bestHarvest; 
